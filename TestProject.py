db40ba7a2acf0debf8cf0c5276f16b945d994582
Alex <alexhutman@gmail.com>
2019-04-29 15:45:37 -0400

    Added k-fold cross validation support

--- TestProject.py
+++ TestProject.py
@@ -22,45 +22,48 @@ import random
 Reads in the given JSON file as outlined in the README.txt file.
 """
 def read_data(file):
-    try:
-        with open(file, 'r') as inf:
-            bitmap = json.load(inf)
+	try:
+		with open(file, 'r') as inf:
+			bitmap = json.load(inf)
 
-        return bitmap
-    except FileNotFoundError as err:
-        print("File Not Found: {0}.".format(err))
+		return bitmap
+	except FileNotFoundError as err:
+		print("File Not Found: {0}.".format(err))
 
 """ 
 Prints out the given "image" (a 2-dimensional array).
 This just replaces any values greater than a threshold with . and otherwise with an X.
 """
 def print_image(img, threshold):
-    for row in img:
-        for pixel in row:
-            print('.' if pixel > threshold else 'X', end='')
-        print()  # Newline at end of the row
+	for row in img:
+		for pixel in row:
+			print('.' if pixel > threshold else 'X', end='')
+		print()  # Newline at end of the row
 
-def kCross(data, percentage):
-    assert percentage >= 0 and percentage <= 1
-
-    train = random.sample(data,round(percentage*len(data)))
-    test = list(set(data)-set(train))
-    return [train, test]
+def kCross(k, data):
+	num = round(len(data)/k)
+	temp_data = data
+	groups = []
+	for i in range(k-1):
+		train = random.sample(temp_data,num)
+		temp_data = list(set(temp_data)-set(train))
+		groups.append(train)
+	groups.append(temp_data)
+	return groups
 
 """ 
 Main entry point.  Assumes all the arguments passed to it are file names.
 For each argument, reads in the file and the prints it out.
 """
 def main():
-    mypath = "C:/DigitProject/DigitDetector/digit_data"
-    files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-    x = [re.search("^input_[0-9]+_[0-9]+_[0-9]+\.json$", i) for i in files]
-    y = [i.group(0) for i in x if i]
+	mypath = "C:/Users/Alex/Dropbox/School Snoot/Spring 2019/AI/DigitDetector/digit_data"
+	files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+	x = [re.search("^input_[0-9]+_[0-9]+_[0-9]+\.json$", i) for i in files]
+	y = [i.group(0) for i in x if i]
 
-    partitions = kCross(y, 0.8)
-    train = partitions[0]
-    test = partitions[1]
-    print(len(train), len(test))
+	partitions = kCross(9, y)
+	#train = partitions[0]
+	#test = partitions[1]
 
 
   
@@ -68,6 +71,6 @@ def main():
 # This is just used to trigger the Python to run the main() method.  The if statement is used so that if this code
 # were imported as a module then everything would load but main() would not be executed.
 if __name__ == "__main__":
-    main()
+	main()
 
 
