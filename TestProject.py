c1bc1019adfc942e7c056d19cf31057ce85be60e
Dave <dvlepore@quinnipiac.edu>
2019-04-29 13:49:46 -0400

    Partitioned data into training and testing

--- TestProject.py
+++ TestProject.py
@@ -17,6 +17,7 @@ from keras.models import Sequential
 from keras.layers import Dense, Activation
 import numpy as np
 import sys
+import random
 """ 
 Reads in the given JSON file as outlined in the README.txt file.
 """
@@ -39,18 +40,27 @@ def print_image(img, threshold):
             print('.' if pixel > threshold else 'X', end='')
         print()  # Newline at end of the row
 
+def kCross(data, percentage):
+    assert percentage >= 0 and percentage <= 1
+
+    train = random.sample(data,round(percentage*len(data)))
+    test = list(set(data)-set(train))
+    return [train, test]
+
 """ 
 Main entry point.  Assumes all the arguments passed to it are file names.
 For each argument, reads in the file and the prints it out.
 """
 def main():
-
-
     mypath = "C:/DigitProject/DigitDetector/digit_data"
     files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
     x = [re.search("^input_[0-9]+_[0-9]+_[0-9]+\.json$", i) for i in files]
     y = [i.group(0) for i in x if i]
-    print(y)
+
+    partitions = kCross(y, 0.8)
+    train = partitions[0]
+    test = partitions[1]
+    print(len(train), len(test))
 
 
   
@@ -59,3 +69,5 @@ def main():
 # were imported as a module then everything would load but main() would not be executed.
 if __name__ == "__main__":
     main()
+
+
